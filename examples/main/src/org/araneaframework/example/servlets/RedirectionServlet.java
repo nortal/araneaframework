c9cfce5033fa5f0db39c66de31ee33ffc35be8be
Taimo Peelo <unserializable@timedrops.us>
2006-03-01 07:33:08 +0000

    templateApp-s goals described a little. localhost:2000 now redirects to real example immediately, instead giving 404 with links
    git-svn-id: file:///root/var/svn/aranea/trunk@24 baba0d3d-6527-0410-ba98-ffd8059025de

---
+++ examples/main/src/org/araneaframework/example/servlets/RedirectionServlet.java
@@ -0,0 +1,24 @@
+package org.araneaframework.example.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Redirection servlet that should be mapped at "/".
+ */
+public class RedirectionServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		service(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		service(request, response);
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("mainExample");
+	}
+}
