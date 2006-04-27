b0bc25f51603b2aa5eb5d59075e5822b1793e37c
ekabanov <Jevgeni.Kabanov@webmedia.eu>
2006-04-27 17:37:35 +0000

    *** empty log message ***
    git-svn-id: file:///root/var/svn/aranea/branches/change_7@401 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/ioc/spring/AraneaSpringDispatcherServlet.java
+++ src/org/araneaframework/ioc/spring/AraneaSpringDispatcherServlet.java
@@ -16,50 +16,124 @@
 
 package org.araneaframework.ioc.spring;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Properties;
 import javax.servlet.ServletException;
-
+import org.araneaframework.core.AraneaRuntimeException;
 import org.araneaframework.servlet.ServletServiceAdapterComponent;
 import org.araneaframework.servlet.core.BaseAraneaDispatcherServlet;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-/**
- * @author Jevgeni Kabanov (ekabanov@webmedia.ee)
- */
 public class AraneaSpringDispatcherServlet extends BaseAraneaDispatcherServlet {
+  public static final String DEFAULT_ARANEA_ROOT = "araneaApplicationRoot";
+  public static final String ARANEA_ROOT_INIT_PARAMETER_NAME = "araneaApplicationRoot";  
   
-  public static final String DEFAULT_ARANEA_ROOT = "applicationRoot";
-  public static final String ARANEA_ROOT_INIT_PARAMETER_NAME = "aranea-application-root";
+  public static final String ARANEA_START = "araneaApplicationStart";  
+  public static final String ARANEA_START_CLASS_PARAMETER = "araneaApplicationStart";
   
-	protected WebApplicationContext beanFactory;
-
-	public void init() throws ServletException {
-    beanFactory = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());   
+  public static final String ARANEA_DEFAULT_CONF_XML = "conf/default-aranea-conf.xml";
+  public static final String ARANEA_DEFAULT_CONF_PROPERTIES = "conf/default-aranea-conf.properties";
+  public static final String ARANEA_CUSTOM_CONF_XML = "/WEB-INF/aranea-conf.xml";
+  public static final String ARANEA_CUSTOM_CONF_PROPERTIES = "/WEB-INF/aranea-conf.properties";
+  
+  protected WebApplicationContext webAppCtx;
+  
+  public void init() throws ServletException {  
+    //Getting the Spring loaded main web application context
+    WebApplicationContext beanFactory = WebApplicationContextUtils.getWebApplicationContext(getServletContext());                     
     
-		super.init();
-	}
+    
+    //Loading default Aranea configuration
+    XmlBeanFactory rootConf = new XmlBeanFactory(new ClassPathResource(ARANEA_DEFAULT_CONF_XML), beanFactory);
+
+    
+    //Loading default properties
+    PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
+    cfg.setLocation(new ClassPathResource(ARANEA_DEFAULT_CONF_PROPERTIES));
+    
+    
+    //Loading custom properties
+    Properties localConf = new Properties();
+    try {
+      if (getServletContext().getResource(ARANEA_CUSTOM_CONF_PROPERTIES) != null)
+        localConf.load(getServletContext().getResourceAsStream( ARANEA_CUSTOM_CONF_PROPERTIES));
+    }
+    catch (IOException e) {
+      throw new ServletException(e);
+    }
+    cfg.setProperties(localConf);
+    cfg.setLocalOverride(true);
+    
+    //Applying properties to default configuration
+    cfg.postProcessBeanFactory(rootConf);
+    
+    
+    //Loading custom configuration 
+    try {
+      if (getServletContext().getResource(ARANEA_CUSTOM_CONF_XML) != null) {    
+        XmlBeanDefinitionReader localConfReader = new XmlBeanDefinitionReader(rootConf);
+        localConfReader.loadBeanDefinitions(new ServletContextResource(getServletContext(), "/WEB-INF/aranea-conf.xml"));
+      }
+    }
+    catch (BeansException e) {
+      throw new AraneaRuntimeException(e);
+    }
+    catch (MalformedURLException e) {
+      throw new AraneaRuntimeException(e);
+    }
+    
+    
+    //Reading the starting widget from an init parameter
+    if (getServletConfig().getInitParameter(ARANEA_START_CLASS_PARAMETER) != null) {
+      Class startClass;
+      try {
+        startClass = Thread.currentThread().getContextClassLoader().loadClass(
+            getServletConfig().getInitParameter(ARANEA_START_CLASS_PARAMETER));
+      }
+      catch (ClassNotFoundException e) {
+        throw new AraneaRuntimeException(e);
+      }
+      
+      rootConf.registerBeanDefinition(ARANEA_START, new RootBeanDefinition(startClass));
+    }
+    
+    //Making a resulting web application context    
+    webAppCtx = new GenericWebApplicationContext(rootConf);    
+    
+    super.init();        
+  }  
   
   protected ServletServiceAdapterComponent buildRootComponent() {
-    String araneaRoot = DEFAULT_ARANEA_ROOT;
-    
+    //Getting the Aranea root component name
+    String araneaRoot = DEFAULT_ARANEA_ROOT;    
     if (getServletConfig().getInitParameter(ARANEA_ROOT_INIT_PARAMETER_NAME) != null)
-      araneaRoot = getServletConfig().getInitParameter(ARANEA_ROOT_INIT_PARAMETER_NAME);
+      araneaRoot = getServletConfig().getInitParameter(ARANEA_ROOT_INIT_PARAMETER_NAME);    
     
+    //Creating the root bean
     ServletServiceAdapterComponent adapter = 
-      (ServletServiceAdapterComponent) beanFactory.getBean(araneaRoot);
+      (ServletServiceAdapterComponent) webAppCtx.getBean(araneaRoot);
     return adapter;
   }
   
   protected Map getEnvironmentEntries() { 
-  	Map result = new HashMap();
-  	result.put(BeanFactory.class, beanFactory);
-    result.put(ApplicationContext.class, beanFactory);
-    result.put(WebApplicationContext.class, beanFactory);    
-  	return result;
+    Map result = new HashMap();
+    result.put(BeanFactory.class, webAppCtx);   
+    result.put(ApplicationContext.class, webAppCtx);
+    result.put(WebApplicationContext.class, webAppCtx);
+    return result;
   }
 }
