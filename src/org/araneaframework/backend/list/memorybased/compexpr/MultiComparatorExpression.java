1689a561795a91bc0ce36cc96c6257535789af09
cvs2svn <cvs2svn@webmedia.eu>
2006-05-16 11:52:22 +0000

    This commit was manufactured by cvs2svn to create tag 'ver-1_2_1_15'.
    git-svn-id: file:///root/var/svn/aranea/tags/ver-1_2_1_15@597 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/backend/list/memorybased/compexpr/MultiComparatorExpression.java
+++ src/org/araneaframework/backend/list/memorybased/compexpr/MultiComparatorExpression.java
@@ -19,7 +19,6 @@ package org.araneaframework.backend.list.memorybased.compexpr;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.araneaframework.backend.list.memorybased.ComparatorExpression;
 import org.araneaframework.backend.list.memorybased.ExpressionEvaluationException;
