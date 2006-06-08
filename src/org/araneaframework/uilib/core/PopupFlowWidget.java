361576818c8fe7c29fbc7d93d5b76f3a131101c1
Taimo Peelo <unserializable@timedrops.us>
2006-06-08 13:12:29 +0000

    *** empty log message ***
    git-svn-id: file:///root/var/svn/aranea/branches/change_39@778 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/uilib/core/PopupFlowWidget.java
+++ src/org/araneaframework/uilib/core/PopupFlowWidget.java
@@ -1,13 +1,14 @@
 package org.araneaframework.uilib.core;
 
+import org.araneaframework.Message;
 import org.araneaframework.Widget;
 
 public class PopupFlowWidget extends StandardPresentationWidget {
   public PopupFlowWidget(Widget widget, MessageFactory messageFactory) {
-    
+
   }
 
-  public class MessageFactory {
-	  
+  public interface MessageFactory {
+    public Message buildMessage(Widget w);
   }
 }
