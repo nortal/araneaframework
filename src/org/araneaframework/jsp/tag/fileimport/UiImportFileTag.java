97c9c8835f9225553de63334df6d73c0eaf40925
cvs2svn <cvs2svn@webmedia.eu>
2006-06-26 12:00:21 +0000

    This commit was manufactured by cvs2svn to create branch 'change_48'.
    git-svn-id: file:///root/var/svn/aranea/branches/change_48@845 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/jsp/tag/fileimport/UiImportFileTag.java
+++ src/org/araneaframework/jsp/tag/fileimport/UiImportFileTag.java
@@ -25,17 +25,17 @@ import org.araneaframework.jsp.tag.UiBaseTag;
  * @author "Toomas Römer" <toomas@webmedia.ee>
  */
 public abstract class UiImportFileTag extends UiBaseTag {
-	protected String includeTemplates;
+	protected String includeGroupName;
 	protected String includeFileName;
 
 	/**
 	 * @jsp.attribute
 	 *   type = "java.lang.String"
 	 *   required = "false"
-	 *   description = "Flag to indicate if the template files should be included too."
+	 *   description = "The name of the group of files that should get included."
 	 */
-	public void setIncludeTemplate(String includeTemplate) throws JspException {
-		this.includeTemplates = (String) evaluate("includeTemplate", includeTemplate, String.class);
+	public void setGroup(String includeTemplate) throws JspException {
+		this.includeGroupName = (String) evaluate("includeTemplate", includeTemplate, String.class);
 	}
 	
 	/**
@@ -48,9 +48,9 @@ public abstract class UiImportFileTag extends UiBaseTag {
 		this.includeFileName = (String) evaluate("file", file, String.class);
 	}
 		
-	public int after(Writer out) throws Exception {
+	public int doEndTag(Writer out) throws Exception {
 		return EVAL_PAGE;
 	}
 	
-	protected abstract void writeHtmlInclude(Writer out, String key) throws Exception;
+	protected abstract void writeContent(Writer out, String key) throws Exception;
 }
