32411c2ea4d2566a2e405b23a9a31e6b2ca99798
cvs2svn <cvs2svn@webmedia.eu>
2006-07-07 12:54:44 +0000

    This commit was manufactured by cvs2svn to create branch 'change_81'.
    git-svn-id: file:///root/var/svn/aranea/branches/change_81@962 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/jsp/support/TagInfo.java
+++ src/org/araneaframework/jsp/support/TagInfo.java
@@ -12,54 +12,47 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-**/
+ **/
 
 package org.araneaframework.jsp.support;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 public class TagInfo implements Serializable {
   protected String tagName;
+
   protected String tagClassName;
-  
+
   public String getTagClassName() {
     return tagClassName;
   }
+
   public void setTagClassName(String tagClassName) {
     this.tagClassName = tagClassName;
   }
+
   public String getTagName() {
     return tagName;
   }
+
   public void setTagName(String tagName) {
     this.tagName = tagName;
   }
-  
-  public static Map makeTagMapping(Element tldElement) {
-    Map result = new HashMap();
-     
-    NodeList tagElements = tldElement.getElementsByTagName("tag");
-       
-     for(int i = 0; i < tagElements.getLength(); i++) {
-       Element tagElement = (Element) tagElements.item(i);
-       
-       TagInfo tag = new TagInfo();
-       
-       NodeList nameElements =  tagElement.getElementsByTagName("name");
-       Element nameElement = (Element) nameElements.item(0);
-       tag.setTagName(nameElement.getFirstChild().getNodeValue());
-       
-       NodeList classElements =  tagElement.getElementsByTagName("tag-class");
-       Element classElement = (Element) classElements.item(0);
-       tag.setTagClassName(classElement.getFirstChild().getNodeValue());  
 
-       result.put(tag.getTagName(), tag);
-     }  
-     
-     return result;  
-   }
+  public static TagInfo readTagInfo(Element tagElement) {
+
+    TagInfo tag = new TagInfo();
+
+    NodeList nameElements = tagElement.getElementsByTagName("name");
+    Element nameElement = (Element) nameElements.item(0);
+    tag.setTagName(nameElement.getFirstChild().getNodeValue());
+
+    NodeList classElements = tagElement.getElementsByTagName("tag-class");
+    Element classElement = (Element) classElements.item(0);
+    tag.setTagClassName(classElement.getFirstChild().getNodeValue());
+
+    return tag;
+  }
 }
