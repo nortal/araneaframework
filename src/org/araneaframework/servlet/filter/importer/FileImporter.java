675a7f1a94ca2f456b85639dadbcab42ddc766a7
cvs2svn <cvs2svn@webmedia.eu>
2006-07-07 12:54:44 +0000

    This commit was manufactured by cvs2svn to create tag 'ver-1_3_2_24'.
    git-svn-id: file:///root/var/svn/aranea/tags/ver-1_3_2_24@963 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/servlet/filter/importer/FileImporter.java
+++ src/org/araneaframework/servlet/filter/importer/FileImporter.java
@@ -16,14 +16,48 @@
 
 package org.araneaframework.servlet.filter.importer;
 
-import java.util.List;
-import org.araneaframework.InputData;
-import org.araneaframework.OutputData;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.araneaframework.servlet.filter.StandardServletFileImportService;
 
 /**
  * @author "Toomas Römer" <toomas@webmedia.ee>
  */
-public interface FileImporter {
-	public void setHeaders(OutputData output);
-	public List importFiles(InputData input);
+public class FileImporter {	
+	
+	/**
+	 * Given a filename, returns the string that can be used for importing via the
+	 * file importing service {@link StandardServletFileImportService}.
+	 * @param fileName
+	 */
+	public final static String getImportString(String fileName) {
+		StringBuffer sb = new StringBuffer("/" + StandardServletFileImportService.FILE_IMPORTER_NAME + "/");
+		sb.append(fileName);
+	
+		return sb.toString();
+	}
+	
+	public final static String getImportString(String fileName, ServletRequest req, ServletResponse res) {
+		HttpServletResponse hres = (HttpServletResponse) res;
+		HttpServletRequest hreq = (HttpServletRequest) req;
+		StringBuffer url = hreq.getRequestURL();
+		
+		/* XXX
+		 * When using jsessionid for session tracking we start getting weird url from
+		 * getRequestURL(). They are in the form of xxx://host/path;jsessionid=abcd. I'd
+		 * expect them as parameters separated by & and starting with ?. Right now i'm stripping
+		 * the url of the jsessionid and it gets added with the encodeURL again. 
+		 */
+		int index = url.indexOf(";");
+		if (index != -1) {
+			url = new StringBuffer(url.substring(0, url.indexOf(";")));
+		}
+		
+		return hres.encodeURL(url
+			.append("/"+StandardServletFileImportService.FILE_IMPORTER_NAME + "/")
+			.append(fileName).toString());
+	}
 }
