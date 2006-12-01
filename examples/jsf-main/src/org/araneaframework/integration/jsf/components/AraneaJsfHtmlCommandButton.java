fc590b14c2a7b7d798369e9d71aa40ca529e2191
Taimo Peelo <unserializable@timedrops.us>
2006-12-01 08:35:29 +0000

    *** empty log message ***
    git-svn-id: file:///root/var/svn/aranea/branches/change_138@2046 baba0d3d-6527-0410-ba98-ffd8059025de

--- examples/jsf-main/src/org/araneaframework/integration/jsf/components/AraneaJsfHtmlCommandButton.java
+++ examples/jsf-main/src/org/araneaframework/integration/jsf/components/AraneaJsfHtmlCommandButton.java
@@ -3,42 +3,45 @@ package org.araneaframework.integration.jsf.components;
 import java.io.IOException;
 import javax.faces.component.html.HtmlCommandButton;
 import javax.faces.context.FacesContext;
+import javax.faces.render.Renderer;
 import javax.servlet.http.HttpServletRequest;
 import org.araneaframework.jsp.UiUpdateEvent;
 import org.araneaframework.jsp.util.JspWidgetCallUtil;
 
 public class AraneaJsfHtmlCommandButton extends HtmlCommandButton {
+	protected void prepareEncode(FacesContext context) {
 
-	public void encodeBegin(FacesContext context) throws IOException {
-		String type = getType();
-		if (type.toLowerCase().equals("submit")) {
+		if (getType().toLowerCase().equals("submit")) {
 			setType("button");
 
 			HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
 			String systemFormId = (String)request.getAttribute("systemFormId");
 			
-			String widgetId =  (String)request.getAttribute("widgetId");
+			String widgetId = (String)request.getAttribute("widgetId");
 			
-			// get surrounding form, if any, figure out its action
-			context.getViewRoot().getChildren();
+			// supposed to be submit button, so construct form action
+			String formAction = context.getViewRoot().getViewId();
 			
 			// now we do not know in which widget we are running but this 
 			// information could be put in and determined from FacesContext
-			UiUpdateEvent event = new UiUpdateEvent("submit", widgetId, null);
+			UiUpdateEvent event = new UiUpdateEvent("submit", widgetId, formAction);
 			
 			setOnclick(JspWidgetCallUtil.getSubmitScriptForEvent(systemFormId, event));
 		}
+	}
+	
+	protected Renderer getRenderer(FacesContext context) {
+		Renderer renderer = super.getRenderer(context);
+		return renderer;
+	}
 
+	public void encodeBegin(FacesContext context) throws IOException {
+		prepareEncode(context);
 		super.encodeBegin(context);
 	}
 
-	public void encodeChildren(FacesContext context) throws IOException {
-		super.encodeChildren(context);
+	public void encodeAll(FacesContext context) throws IOException {
+		prepareEncode(context);
+		super.encodeAll(context);
 	}
-
-	public void encodeEnd(FacesContext context) throws IOException {
-
-		super.encodeEnd(context);
-	}
-
 }
