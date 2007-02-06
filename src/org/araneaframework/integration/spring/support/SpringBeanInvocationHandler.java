dcb9455861113940f4f4ebe3a9017d349c4515d9
Alar Kvell <alar.kvell@eesti.ee>
2007-02-06 14:09:43 +0000

    Now works with JDK 1.3
    git-svn-id: file:///root/var/svn/aranea/branches/change_156@2283 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/integration/spring/support/SpringBeanInvocationHandler.java
+++ src/org/araneaframework/integration/spring/support/SpringBeanInvocationHandler.java
@@ -1,8 +1,26 @@
+/**
+ * Copyright 2006 Webmedia Group Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
 package org.araneaframework.integration.spring.support;
+
 import java.io.Serializable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.araneaframework.Environment;
 import org.springframework.beans.factory.BeanFactory;
 
@@ -24,7 +42,7 @@ public class SpringBeanInvocationHandler implements InvocationHandler, Serializa
     try {
       return method.invoke(bean, args);
     } catch (InvocationTargetException ex){
-      throw ex.getCause();
+      throw ExceptionUtils.getCause(ex);
     }
   }
 }
