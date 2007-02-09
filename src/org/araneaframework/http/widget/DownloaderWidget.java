f978039ac21047ac2f2c8420de6ce2bd711ce74e
Alar Kvell <alar.kvell@eesti.ee>
2007-02-09 09:56:32 +0000

    Change 160 Version ver-2_1_1_16 DownloaderWidget fixes
    git-svn-id: file:///root/var/svn/aranea/branches/latest@2323 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/http/widget/DownloaderWidget.java
+++ src/org/araneaframework/http/widget/DownloaderWidget.java
@@ -16,17 +16,44 @@
 
 package org.araneaframework.http.widget;
 
-import org.araneaframework.http.service.DownloaderService;
+import javax.servlet.http.HttpServletResponse;
+import org.araneaframework.InputData;
+import org.araneaframework.OutputData;
+import org.araneaframework.Path;
+import org.araneaframework.core.Assert;
+import org.araneaframework.core.BaseApplicationWidget;
+import org.araneaframework.http.util.ServletUtil;
 
 /**
  * Widget that serves content.
  * 
  * @author Alar Kvell (alar@araneaframework.org)
  */
-public class DownloaderWidget extends DownloaderService {
+public class DownloaderWidget extends BaseApplicationWidget {
+
+  protected byte[] data;
+  protected String contentType;
 
   public DownloaderWidget(byte[] data, String contentType) {
-    super(data, contentType);
+    Assert.notNullParam(data, "data");
+    Assert.notEmptyParam(contentType, "contentType");
+    this.data = data;
+    this.contentType = contentType;
+  }
+
+  public byte[] getData() {
+    return data;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  protected void action(Path path, InputData input, OutputData output) throws Exception {
+    HttpServletResponse response = ServletUtil.getResponse(output);
+    response.setContentType(getContentType());
+    response.setContentLength(getData().length);
+    response.getOutputStream().write(getData());
   }
 
 }
