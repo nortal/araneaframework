e31efe53f28e27fdca80eb8c1be093af4daa3cbd
Taimo Peelo <unserializable@timedrops.us>
2007-05-16 20:41:48 +0000

    nicer indent of TcdAndTLDMerger output. Moved manifest generation to one place in buildfile.
    git-svn-id: file:///root/var/svn/aranea/branches/change_392@3039 baba0d3d-6527-0410-ba98-ffd8059025de

--- etc/src/org/araneaframework/buildutil/TcdAndTldMerger.java
+++ etc/src/org/araneaframework/buildutil/TcdAndTldMerger.java
@@ -19,8 +19,8 @@ package org.araneaframework.buildutil;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -37,8 +37,8 @@ import javax.xml.transform.stream.StreamResult;
 import org.apache.xerces.dom.DocumentImpl;
 import org.apache.xerces.dom.NodeImpl;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
@@ -136,7 +136,7 @@ public class TcdAndTldMerger {
 		Result out = new StreamResult(ostream);
 
 		TransformerFactory xformFactory = TransformerFactory.newInstance();
-		
+
 		try {
 			Transformer idTransformer = xformFactory.newTransformer();
 			
@@ -150,8 +150,10 @@ public class TcdAndTldMerger {
 				idTransformer.setOutputProperty(
 						OutputKeys.DOCTYPE_SYSTEM, systemValue
 				);
+				
+				idTransformer.setOutputProperty(OutputKeys.INDENT, "yes" );
 			}
-
+			
 			idTransformer.transform(input, out);
 			
 		} catch (Exception e) {
