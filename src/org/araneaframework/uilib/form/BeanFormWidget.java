d471ea19593b1e419139e2ab67cc6173c2a321e9
Taimo Peelo <unserializable@timedrops.us>
2007-06-25 08:54:58 +0000

    Change 446 Version ver-2_3_1_5 BeanFormWidget
    git-svn-id: file:///root/var/svn/aranea/branches/latest@3334 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/uilib/form/BeanFormWidget.java
+++ src/org/araneaframework/uilib/form/BeanFormWidget.java
@@ -75,13 +75,31 @@ public class BeanFormWidget extends FormWidget {
     return super.addElement(elementName, labelId, control, inferDataType(elementName, mandatory), initialValue, mandatory);
   }
   
+  /**
+   * Writes values from this {@link BeanFormWidget} into bean. 
+   * @deprecated use {@link BeanFormWidget#writeToBean(Object)} instead
+   * @return bean filled with form data
+   */
   public Object readBean(Object bean) {
+    return writeToBean(bean);
+  }
+  
+  public Object writeToBean(Object bean) {
     BeanFormReader reader = new BeanFormReader(this);
     reader.readFormBean(bean);
     return bean;
   }
 
+  /**
+   * Reads values from bean into this {@link BeanFormWidget}.
+   * @deprecated use {@link BeanFormWidget#readBean(Object)} instead
+   * @param bean
+   */
   public void writeBean(Object bean) {
+    readFromBean(bean);
+  }
+  
+  public void readFromBean(Object bean) {
     BeanFormWriter writer = new BeanFormWriter(beanClass);
     writer.writeFormBean(this, bean);
   }
