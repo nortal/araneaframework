e87853c9aec810247ac6c86321c5539d29fbaed5
Taimo Peelo <unserializable@timedrops.us>
2008-01-17 11:40:55 +0000

    removed combotextinputcontrol
    git-svn-id: file:///root/var/svn/aranea/branches/change_578@4088 baba0d3d-6527-0410-ba98-ffd8059025de

--- examples/main/src/org/araneaframework/example/main/web/menu/MenuWidget.java
+++ examples/main/src/org/araneaframework/example/main/web/menu/MenuWidget.java
@@ -23,7 +23,6 @@ import org.araneaframework.core.ProxyEventListener;
 import org.araneaframework.example.common.framework.TemplateMenuWidget;
 import org.araneaframework.example.main.SecurityContext;
 import org.araneaframework.example.main.release.ReleaseWidget;
-import org.araneaframework.example.main.release.demos.ComboTextInputDemoWidget;
 import org.araneaframework.example.main.release.demos.DemoActionPollWidget;
 import org.araneaframework.example.main.release.demos.DemoAutoCompletionWidget;
 import org.araneaframework.example.main.release.demos.DemoContextMenuWidget;
@@ -119,7 +118,6 @@ private MenuItem araneaMenu;
 		araneaMenu.addMenuItem(new MenuItem("Seamless_Validation", SeamlessFormValidationDemoWidget.class));
 		araneaMenu.addMenuItem(new MenuItem("Tabs_Demo", DemoNewTabWidget.class));
 		araneaMenu.addMenuItem(new MenuItem("TreeComponent", SimpleTreeWidget.class));
-		araneaMenu.addMenuItem(new MenuItem("ComboTextInput", ComboTextInputDemoWidget.class));
 		araneaMenu.addMenuItem(new MenuItem("Serverside_Polling", DemoActionPollWidget.class));
 
 		// "Management"
