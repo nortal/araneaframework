56e880e92ffa90d98f9d059f4f204d462012160f
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2008-10-27 08:10:56 +0000

    Change 705 Version ver-4_1_1_2 Spring beans injections using @Resource
    git-svn-id: file:///root/var/svn/aranea/branches/latest@4571 baba0d3d-6527-0410-ba98-ffd8059025de

--- tests/src/org/araneaframework/tests/mock/MockEnvironment.java
+++ tests/src/org/araneaframework/tests/mock/MockEnvironment.java
@@ -25,15 +25,15 @@ import org.araneaframework.uilib.ConfigurationContext;
 
 
 /**
- * This class implemets {@link UiLibEnviroment} interface
- * providing a straightforward implementation, which basicly returns the
+ * This class implements {@link Environment} interface
+ * providing a straightforward implementation, which basically returns the
  * arguments passed without any specific processing. It is used primarily for testing.
  * 
  * @author Jevgeni Kabanov (ekabanov <i>at</i> araneaframework <i>dot</i> org)
  * 
  */
 public class MockEnvironment extends BaseEnvironment {
-  private Map contexts = new HashMap();
+  protected Map contexts = new HashMap();
 
   public MockEnvironment() {
     contexts.put(ConfigurationContext.class, new MockConfiguration());
