47729a4fc3b42766b91d2404c067e36982e1e10a
martti <martti@webmedia.eu>
2008-11-04 14:45:28 +0000

    Change 709 Version ver-3_1_1_12 Aranea 1.2 M2 fixes
    git-svn-id: file:///root/var/svn/aranea/branches/branch-3_1_1@4591 baba0d3d-6527-0410-ba98-ffd8059025de

--- etc/js/aranea/aranea.js
+++ etc/js/aranea/aranea.js
@@ -1034,7 +1034,7 @@ AraneaPage.FormBackgroundValidationRegionHandler.prototype = {
     Aranea.UI.markFEContentStatus(result.valid, labelSpan);
     
     if (result.valid || result.clientRenderText) {
-      document.getElementsByClassName('aranea-formelementerrors ' + result.formElementId).each(
+      $$('.aranea-formelementerrors ' + result.formElementId).each(
         function(e) { e.remove() }
       );
     } 
--- src/org/araneaframework/jsp/tag/fileimport/ImportScriptsHtmlTag.java
+++ src/org/araneaframework/jsp/tag/fileimport/ImportScriptsHtmlTag.java
@@ -29,36 +29,32 @@ import org.araneaframework.jsp.util.JspUtil;
  *   description = "Imports js files"
  */
 public class ImportScriptsHtmlTag extends BaseFileImportTag {
-	public static final String DEFAULT_GROUP_NAME = "defaultScripts";
-	
-	public int doStartTag(Writer out) throws Exception {
-		// if filename specified we include the file
-		if (includeFileName != null) {
-			writeContent(out, includeFileName);
-		}
-		// if groupname specified we include the group
-		else if (includeGroupName != null) {
-			writeContent(out, includeGroupName);
-		}
-		// we default to the default group name
-		else {
-			writeContent(out, DEFAULT_GROUP_NAME);
-		}
-		return EVAL_BODY_INCLUDE;
-	}
-	
-	protected void writeContent(Writer out, String srcFile) throws Exception {
-		writeHtmlScriptsInclude(out, FileImportUtil.getImportString(srcFile, pageContext.getRequest()));
-		out.write("\n");
-	}
-	
-	public static void writeHtmlScriptsInclude(Writer out, String srcFile) throws Exception {
-		JspUtil.writeOpenStartTag(out, "script");
-		JspUtil.writeAttribute(out, "language", "JavaScript1.2");
-		JspUtil.writeAttribute(out, "type", "text/javascript");
-		JspUtil.writeAttribute(out, "src", srcFile, false);
-		JspUtil.writeCloseStartTag(out);
-		JspUtil.writeEndTag(out, "script");
-		out.write("\n");
-	}
+
+  public int doStartTag(Writer out) throws Exception {
+    // if filename specified we include the file
+    if (includeFileName != null) {
+      writeContent(out, includeFileName);
+    } else if (includeGroupName != null) {
+      writeContent(out, includeGroupName + GROUP_JS_SUFFIX);
+    } else {
+      writeContent(out, DEFAULT_GROUP_NAME + GROUP_JS_SUFFIX);
+    }
+    return EVAL_BODY_INCLUDE;
+  }
+
+  protected void writeContent(Writer out, String srcFile) throws Exception {
+    writeHtmlScriptsInclude(out, FileImportUtil.getImportString(srcFile,
+        pageContext.getRequest()));
+  }
+
+  public static void writeHtmlScriptsInclude(Writer out, String srcFile)
+      throws Exception {
+    JspUtil.writeOpenStartTag(out, "script");
+    JspUtil.writeAttribute(out, "language", "JavaScript1.2");
+    JspUtil.writeAttribute(out, "type", "text/javascript");
+    JspUtil.writeAttribute(out, "src", srcFile, false);
+    JspUtil.writeCloseStartTag(out);
+    JspUtil.writeEndTag(out, "script");
+    out.write("\n");
+  }
 }
