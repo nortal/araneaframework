4e292fc8b0bb19b369974f072628db1e293fdab9
martti <martti@webmedia.eu>
2009-08-19 09:41:28 +0000

    Change 812 Version ver-4_1_1_28 JSP EL evaluation memory leak fix
    git-svn-id: file:///root/var/svn/aranea/branches/latest@5095 baba0d3d-6527-0410-ba98-ffd8059025de

--- src/org/araneaframework/jsp/tag/BaseTag.java
+++ src/org/araneaframework/jsp/tag/BaseTag.java
@@ -223,9 +223,8 @@ public class BaseTag implements Tag, TryCatchFinally, ContainedTagInterface {
    * Evaluates attribute value.
    */
   protected Object evaluate(String attributeName, String attributeValue, Class classObject) throws JspException {
-    boolean elEnabled = ConfigurationContextUtil.isELEvaluationEnabled(getConfiguration());
-    return !elEnabled ? attributeValue : ExpressionEvaluatorManager.evaluate(
-        attributeName, attributeValue, classObject, this, this.pageContext);
+    return ConfigurationContextUtil.getResolver(getConfiguration()).evaluate(
+        attributeName, attributeValue, classObject, this.pageContext);
   }
 
   /* ***********************************************************************************
