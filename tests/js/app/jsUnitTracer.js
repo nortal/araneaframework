9366bb706f449c33ed783f15dcaf2bc2edac3345
martti <martti@webmedia.eu>
2011-07-27 10:41:21 +0000

    jsunit 2.2
    git-svn-id: file:///root/var/svn/aranea/branches/change_913@5545 baba0d3d-6527-0410-ba98-ffd8059025de

--- tests/js/app/jsUnitTracer.js
+++ tests/js/app/jsUnitTracer.js
@@ -1,83 +1,48 @@
-/* @author Edward Hieatt, edward@jsunit.net */
-
-function jsUnitTracer() {
-  this._traceWindow        = null;
-  this.TRACE_LEVEL_WARNING = 1;
-  this.TRACE_LEVEL_INFO    = 2;
-  this.TRACE_LEVEL_DEBUG   = 3;
-  this.popupWindowsBlocked = false;
+function JsUnitTraceLevel(levelNumber, color) {
+    this._levelNumber = levelNumber;
+    this._color = color;
 }
 
-jsUnitTracer.prototype.initialize = function () 
-{
-  if (this._traceWindow != null && top.testManager.closeTraceWindowOnNewRun.checked)
-    this._traceWindow.close();
-
-  this._traceWindow = null;
+JsUnitTraceLevel.prototype.matches = function(otherTraceLevel) {
+    return this._levelNumber >= otherTraceLevel._levelNumber;
 }
 
-jsUnitTracer.prototype.finalize = function () 
-{
-  if (this._traceWindow!=null) {
-    this._traceWindow.document.write('<\/body>\n<\/html>');
-    this._traceWindow.document.close();
-  }
+JsUnitTraceLevel.prototype.getColor = function() {
+    return this._color;
 }
 
-jsUnitTracer.prototype.warn = function () 
-{
-  this._trace(arguments[0], arguments[1], this.TRACE_LEVEL_WARNING);
-}
-
-jsUnitTracer.prototype.inform = function () 
-{
-  this._trace(arguments[0], arguments[1], this.TRACE_LEVEL_INFO);
-}
-
-jsUnitTracer.prototype.debug = function () 
-{
-  this._trace(arguments[0], arguments[1], this.TRACE_LEVEL_DEBUG);
-}
-
-jsUnitTracer.prototype._trace = function (message, value, traceLevel) 
-{
-  if (this._getChosenTraceLevel() >= traceLevel) {
-    var traceString = message;
-    if (value)
-      traceString += ': ' + value;
-    this._writeToTraceWindow(traceString, traceLevel);
-  }
-}
-
-jsUnitTracer.prototype._getChosenTraceLevel = function () 
-{
-  return eval(top.testManager.traceLevel.value);
-}
-
-jsUnitTracer.prototype._writeToTraceWindow  = function (traceString, traceLevel) 
-{
-  var htmlToAppend = '<p class="jsUnitDefault">' + traceString + '<\/p>\n';
-  this._getTraceWindow().document.write(htmlToAppend);
-}
-
-jsUnitTracer.prototype._getTraceWindow = function () 
-{
-  if (this._traceWindow == null && !this.popupWindowsBlocked) {
-    this._traceWindow = window.open('','','width=600, height=350,status=no,resizable=yes,scrollbars=yes');
-    if (!this._traceWindow) {
-      this.popupWindowsBlocked = true;
+JsUnitTraceLevel.findByLevelNumber = function(levelNumber) {
+    switch (levelNumber) {
+        case 0: return JsUnitTraceLevel.NONE;
+        case 1: return JsUnitTraceLevel.WARNING;
+        case 2: return JsUnitTraceLevel.INFO;
+        case 3: return JsUnitTraceLevel.DEBUG;
     }
-    else {
-      var resDoc = this._traceWindow.document;
-      resDoc.write('<html>\n<head>\n<link rel="stylesheet" href="css/jsUnitStyle.css">\n<title>Tracing - JsUnit<\/title>\n<head>\n<body>');
-      resDoc.write('<h2>Tracing - JsUnit<\/h2>\n');
-    }
-  }
-  return this._traceWindow;
+    return null;
 }
 
-if (xbDEBUG.on)
-{
-  xbDebugTraceObject('window', 'jsUnitTracer');
+JsUnitTraceLevel.NONE = new JsUnitTraceLevel(0, null);
+JsUnitTraceLevel.WARNING = new JsUnitTraceLevel(1, "#FF0000");
+JsUnitTraceLevel.INFO = new JsUnitTraceLevel(2, "#009966");
+JsUnitTraceLevel.DEBUG = new JsUnitTraceLevel(3, "#0000FF");
+
+function JsUnitTracer(testManager, params) {
+    this._testManager = testManager;
+    this._params = params;
 }
 
+JsUnitTracer.prototype.warn = function() {
+    this._trace(arguments[0], arguments[1], JsUnitTraceLevel.WARNING);
+}
+
+JsUnitTracer.prototype.inform = function() {
+    this._trace(arguments[0], arguments[1], JsUnitTraceLevel.INFO);
+}
+
+JsUnitTracer.prototype.debug = function() {
+    this._trace(arguments[0], arguments[1], JsUnitTraceLevel.DEBUG);
+}
+
+JsUnitTracer.prototype._trace = function(message, value, traceLevel) {
+    this._testManager.addTraceData(message, value, traceLevel);
+}
